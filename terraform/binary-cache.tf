5a93525d800aef90cb9177cb8c89451e7de977c0
Alessandro Sappia <alessandro.sappia@tii.ae>
2026-05-18 11:22:01 +0400

    Update list of users
    Signed-off-by: Alessandro Sappia <alessandro.sappia@tii.ae>

--- terraform/binary-cache.tf
+++ terraform/binary-cache.tf
@@ -31,7 +31,7 @@ module "binary_cache_vm" {
 
   virtual_machine_custom_data = join("\n", ["#cloud-config", yamlencode({
     users = [
-      for user in toset(["bmg", "flokli", "hrosten", "jrautiola", "vjuntunen", "cazfi", "fayad", "john"]) : {
+      for user in toset(["uday", "john", "alessandro"]) : {
         name                = user
         sudo                = "ALL=(ALL) NOPASSWD:ALL"
         ssh_authorized_keys = local.ssh_keys[user]
